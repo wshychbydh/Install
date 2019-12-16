2d29c906f237f753d6e73b00d6ce7208ecbd19dc
ycb <yangchuanbo@sudiyi.cn>
2019-12-16 17:22:18 +0800

    fix permission error

--- app/src/main/java/com/eye/cool/install/ui/PermissionActivity.kt
+++ app/src/main/java/com/eye/cool/install/ui/PermissionActivity.kt
@@ -1,15 +1,19 @@
 package com.eye.cool.install.ui
 
 import android.annotation.TargetApi
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.view.KeyEvent
+import android.view.View
+import android.view.WindowManager
 import com.eye.cool.install.R
 
 /**
@@ -17,10 +21,12 @@ import com.eye.cool.install.R
  * Created cool on 2018/4/16.
  */
 @TargetApi(Build.VERSION_CODES.M)
-internal class PermissionActivity : DialogActivity() {
+internal class PermissionActivity : Activity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    invasionStatusBar(this)
 
     if (sRequestPermissionListener == null) {
       showInstallSettingDialog()
@@ -99,6 +105,19 @@ internal class PermissionActivity : DialogActivity() {
       intent.putExtra(REQUEST_PERMISSIONS, permissions)
       intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
       context.startActivity(intent)
+    }
+
+    /**
+     * Set the content layout full the StatusBar, but do not hide StatusBar.
+     */
+    private fun invasionStatusBar(activity: Activity) {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        val window = activity.window
+        val decorView = window.decorView
+        decorView.systemUiVisibility = (decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = Color.TRANSPARENT
+      }
     }
   }
 }
